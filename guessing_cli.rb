4de40743511092d60466451606421d757696eb3c
John Mark Redding <johnmarkredding@gmail.com>
2018-11-16 21:28:35 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,7 +2,7 @@
 def run_guessing_game
   usr_input = ""
   until usr_input == "exit"
-    comp_guess = rand(6)
+    comp_guess = rand(1, 6)
     usr_input = gets.chomp
     if (usr_input != "exit")
       puts (usr_input.to_i == comp_guess) ? "You guessed the correct number!":"The computer guessed #{comp_guess}."
