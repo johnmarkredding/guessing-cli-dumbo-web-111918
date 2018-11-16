91a870c2ff24ba7e41fde7930f14228da54a0550
John Mark Redding <johnmarkredding@gmail.com>
2018-11-16 21:43:43 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,6 +1,6 @@
 # Code your solution here!
 def run_guessing_game
-  correct_ans = false;
+  correct_ans = false
   usr_input = ""
   puts "Guess a number between 1 and 6."
   while usr_input != "exit" && !correct_ans
@@ -8,7 +8,10 @@ def run_guessing_game
     comp_guess = prng.rand(1..6)
     usr_input = gets.chomp
     if (usr_input != "exit")
-      puts (usr_input.to_i == comp_guess) ? "You guessed the correct number!":"The computer guessed #{comp_guess}."
+      if (usr_input.to_i == comp_guess)
+        puts "You guessed the correct number!"
+        correct_ans = true
+        "The computer guessed #{comp_guess}."
     else
       puts "Goodbye!"
     end
